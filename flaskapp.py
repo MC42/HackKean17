14cccaf0cf2eeb3a6b804bed218e979b7db38cf1
Tim Flynn <tjflynn@theflynn.org>
2017-04-03 12:02:30 -0400

    Update + custom error codes for web server.

--- flaskapp.py
+++ flaskapp.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python2
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import requests
 import sqlite3
 import sys
-import datetime
+import datetime, time
 import os.path
 
+#Keep track of how many requests are made.
 global tbaRequests
 tbaRequests = 0
 
+#Set current year for thing.
 year = str(datetime.datetime.today().year)
+#Set how wide the table is, equal to bestWorst*2 + 1 (team)
 bestWorst = 4
 
 baseURL = 'http://www.thebluealliance.com/api/v2/'
 header = {'X-TBA-App-Id': 'tjf:particleprescouter:hackkean3'} #Yay, version strings....
-
-conn = sqlite3.connect(':memory:',detect_types=sqlite3.PARSE_DECLTYPES| sqlite3.PARSE_COLNAMES, check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute('CREATE TABLE `MATCHES` (	`KEY`	TEXT,	`SCORE`	INTEGER);')
 
 def matchLogic(match, team, key):
 	team="frc"+str(team)
@@ -68,23 +66,29 @@ def tbaIncGet():
 	return str(tbaRequests)
 
 def getEvent(teams, eventCode):
+	global conn
+	conn = sqlite3.connect(':memory:',detect_types=sqlite3.PARSE_DECLTYPES| sqlite3.PARSE_COLNAMES, check_same_thread=False)
+	global cursor
+	cursor = conn.cursor()
+
+	cursor.execute('CREATE TABLE `MATCHES` (	`KEY`	TEXT,	`SCORE`	INTEGER);')
 	tbaIncrement()
 	finalOut ="<link href=\"https://fonts.googleapis.com/css?family=Droid+Sans|Righteous\" rel=\"stylesheet\"><style>table{font-size:14px;}th{text-align:left;}	.center{margin: auto;    border: 3px solid #ff6600;    padding: 10px;border-radius:10px;}body{font-family:'Droid Sans';}h1,h3{font-family:'Righteous';line-height: 70%;}</style>"
 	finalOut += "<h1 style=\"text-align:center\">" + eventCode.upper() + " Event</h1>"
 	finalOut += "<table class=\"center\" style=\"width:100%;\">"
+
 	#Generates The Header for the Event
 	finalOut+="<tr><th>Team No.</th>"
 	for num in range(bestWorst):
 		finalOut+="<th>#" + str(num+1) + " Best Match</th>"
 	for num in reversed(range(bestWorst)):
 		finalOut+="<th>#" + str(num+1) + " Worst Match</th>"
-
 	finalOut+="</tr>"
 
-	#Geneates the data for each team at the event.
+	#Generates the data for each team at the event.
 	for i in teams:	
 		tbaIncrement()
-		myRequest = (baseURL + 'team/frc'+ str(i) + '/2017/events')
+		myRequest = (baseURL + 'team/frc'+ str(i) + '/'+ year + '/events')
 		response = requests.get(myRequest, headers=header)
 		jsonified = response.json()
 
@@ -106,6 +110,8 @@ def getEvent(teams, eventCode):
 		cursor.execute("DELETE FROM MATCHES;")  #CLEANUP FOR NEXT TEAM
 	finalOut+="</table>"
 
+	finalOut+="<h6>Page generated at: " + str(datetime.datetime.now()) + "</h6>"
+
 	#Archives Data to File
 	f = open(("./export/" + eventCode + ".html"), "w+")
 	f.write(finalOut)
@@ -123,6 +129,22 @@ def frontPage():
 	finalOut+="<div class=\"center\"><table style=\"width:100%;\">"
 	finalOut+="<tr><th>Week No.</th><th>Event Short Name</th><th>Event Location</th></tr>"
 	for t in jsonified:
+		#Week off-by-one Corrector
+		if t['week'] in range(-1,10):
+			t['week']+=1
+
+		#Preseasons
+		if time.strptime(str(t['start_date']), "%Y-%m-%d") < time.strptime( year + "-03-01", "%Y-%m-%d"):
+			t['week'] = "Preseason"
+
+		#Offseasons
+		if time.strptime(str(t['start_date']), "%Y-%m-%d") > time.strptime(year + "-04-26", "%Y-%m-%d"):
+			t['week'] = "Offseason"
+		#Champs Codes
+		if( t['event_code'] == "cmptx" or t['event_code'] == "cmpmo"):
+			print(t['event_code'])
+			t['week'] = "Champs"
+
 		finalOut+="<tr><td>"+ str(t['week'])+ "</td><td><a href=\"events/" + t['key'] + "\">" + t['short_name'] + " (" + t['event_code'].upper() + ")</a></td><td>" + t['location'] + "</td></tr>"
 	finalOut+="</table>"
 	finalOut+="<h6 style=\"text-align:center\">" + tbaIncGet() + " Requests to TBA (And Counting!)</h6></div>"
@@ -135,6 +157,7 @@ def file_len(fname):
     return i + 1
 
 def savePage():
+	print("Saving entire damn site to static!")
 	myRequest = (baseURL + "events/"+ str(year))
 	response = requests.get(myRequest, headers=header)
 	jsonified = response.json()
@@ -144,11 +167,17 @@ def savePage():
 			f.write(getEvent(getTeamsAtEvent(t['key']), t['key']))
 			print("Updating " + t['key'].upper() + " via cache view.")
 			f.close()
+		elif (open("./export/" + t['key'] + ".html").read() == ""):
+			f = open(("./export/" + t['key'] + ".html"), "w+")
+			f.write(getEvent(getTeamsAtEvent(t['key']), t['key']))
+			print("Updating " + t['key'].upper() + " via cache view.")
+			f.close()
 
 def statsTest():
 	finalOut = "<link href=\"https://fonts.googleapis.com/css?family=Droid+Sans\" rel=\"stylesheet\"><link href=\"https://fonts.googleapis.com/css?family=Righteous\" rel=\"stylesheet\"> <style>body{font-family:'Droid Sans';}h1,h3{font-family:'Righteous';line-height: 70%;}.center{margin: auto;width: 60%;border:5px solid #ff6600;padding:10px;border-radius:10px;}</style>"
 	finalOut+="<h4>" + tbaIncGet()  +" requests to The Blue Alliance and counting!</h4>"
 	finalOut+="<h4>" + str(file_len("./flaskapp.py")) + " lines of code in this project.</h4>"
+	finalOut+="<form action=\"admin/save/\" method=\"post\"><button name=\"foo\" value=\"upvote\">Save Entire Site to Static</button></form>"
 
 	return finalOut
 	
@@ -178,10 +207,19 @@ def getEvents():
     return frontPage()
 
 #SAVE EVERYTHING
-@app.route('/save')
+@app.route('/admin/save/', methods=['POST'])
 def saveEvents():
     savePage()
-    return "Saved!"
+    return ''
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+
